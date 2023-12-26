84be6a098e9f596712c7c81b5812de5328f8a0d1
samiterefe <samuelterefe2@gmail.com>
2023-12-26 07:30:46 -0500

    working with signup backend

--- api/server.js
+++ api/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-const app = express();
-import useRoutes from './routes/user.route.js';
+
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import path from 'path';
+
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,8 +16,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const app = express();
+
+app.use(express.json());
+
 app.listen(3000, ()=>{
     console.log("server listening on port 3000");
 });
 
-app.use('/api/user', useRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
