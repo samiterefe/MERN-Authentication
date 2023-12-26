6cce3a46f9a60e006f2cacf3cac903eb63a59781
samiterefe <samuelterefe2@gmail.com>
2023-12-26 02:43:37 -0500

    express routes working

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+
+export const userController = (req, res)=>{
+    res.json({
+     message: "api part is working"
+    })
+ }
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
+import useRoutes from './routes/user.route.js';
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,4 +15,6 @@ mongoose
   });
 app.listen(3000, ()=>{
     console.log("server listening on port 3000");
-})
+});
+
+app.use('/api/user', useRoutes)
