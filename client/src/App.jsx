b64a3077a75a0f8e79d74df9562cb23b43b1918b
samiterefe <samuelterefe2@gmail.com>
2023-12-26 01:26:31 -0500

    working with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 export default function App() {
   return (
-    <div className='underline'> <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+   <>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile/>} />
+
+    </Routes>
+
+   </BrowserRouter>
+   </>
   )
 }
