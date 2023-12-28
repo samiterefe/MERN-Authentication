c8c45787966995da4facdb3d099621bcbd7b9609
samiterefe <samuelterefe2@gmail.com>
2023-12-28 15:22:32 -0500

    store redux

--- client/src/pages/redux/store.js
+++ client/src/pages/redux/store.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-function store() {
-  return (
-    <div>store</div>
-  )
-}
+const rootReducer = combineReducers({ user: userReducer });
 
-export default store
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
